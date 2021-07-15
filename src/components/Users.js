e52d0ec78b6cc34b939193b089f14ac099fa5eb3
Stefano Savanelli <stefano@savanelli.it>
2021-07-15 17:17:05 +0200

    users nested routes

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,77 @@
-import React from 'react'
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom"
 
-export default () => (
-  <h1>Users</h1>
-)
+
+const Show = () => {
+  const { id } = useParams();
+  const [user, setUser] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/users/" + id)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+  return (
+    <>
+      <h3>Requested topic ID: {id}</h3>
+      <pre>
+        {JSON.stringify(user,null,2)}
+      </pre>
+    </>
+  )
+}
+
+const List = () => {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h3>Users list</h3>
+      <table>
+        {users.map(({ name, id }) => (
+          <tr key={id} >
+            <td>{name}</td>
+            <td><Link to={'users/' + id}>Show</Link></td>
+          </tr>
+        ))}
+      </table>
+    </>
+  );
+}
+
+export default () => {
+  let match = useRouteMatch()
+  return (
+    <Switch>
+      <Route path={`${match.path}/:id`}>
+        <Show />
+      </Route>
+      <Route path={match.path}>
+        <List resource="users" />
+      </Route>
+    </Switch>
+  )
+}
